bc50a4a773ef934214da6b6d8f4d8448cbaa65c3
SYGasanov <sygasanov@get-lab.ru>
2024-09-19 18:09:17 +0300

    Add data fetching to component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,22 @@
 import { Box } from "@mantine/core";
+import useSWR from "swr";
+import { ENDPOINT } from "./constants";
+
+const fetcher = (url: string) => {
+  fetch(`${ENDPOINT}/${url}`)
+    .then((r) => r.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => console.error("Failed to fetch ", err));
+};
 
 function App() {
-  return <Box>Hello world</Box>;
+  const { data, mutate } = useSWR("api/todos", fetcher);
+
+  console.log(data);
+
+  return <Box>{JSON.stringify(data)}</Box>;
 }
 
 export default App;
