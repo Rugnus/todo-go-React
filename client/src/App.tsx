f6596fd24239ddf72d5536a4eb1652419a4317f3
SYGasanov <sygasanov@get-lab.ru>
2024-09-21 10:10:11 +0300

    add form to post todos

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,42 @@
-import { Box } from "@mantine/core";
+import { Box, List, ListItem } from "@mantine/core";
 import useSWR from "swr";
+import AddTodo from "./components/AddTodo";
 import { ENDPOINT } from "./constants";
 
+export interface Todo {
+  id: number;
+  title: string;
+  body: string;
+  done: boolean;
+}
+
 const fetcher = (url: string) => {
-  fetch(`${ENDPOINT}/${url}`)
+  return fetch(`${ENDPOINT}/${url}`)
     .then((r) => r.json())
-    .then((data) => {
-      console.log(data);
-    })
     .catch((err) => console.error("Failed to fetch ", err));
 };
 
 function App() {
-  const { data, mutate } = useSWR("api/todos", fetcher);
+  const { data, mutate } = useSWR<Todo[]>("api/todos", fetcher);
 
-  console.log(data);
-
-  return <Box>{JSON.stringify(data)}</Box>;
+  return (
+    <Box
+      sx={(theme) => ({
+        padding: "2rem",
+        width: "100%",
+        maxWidth: "40rem",
+        margin: "0 auto",
+      })}
+    >
+      {JSON.stringify(data)}
+      <List spacing="xs" size="sm" mb={12} center>
+        {data?.map((todo) => {
+          return <ListItem key={`todo__${todo.id}`}>{todo.title}</ListItem>;
+        })}
+      </List>
+      <AddTodo mutate={mutate} />
+    </Box>
+  );
 }
 
 export default App;
